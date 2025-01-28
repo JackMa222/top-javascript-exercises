ec3888ea33bf29d11cc5fe532cf8531012e1c37b
JackMa222 <128107480+JackMa222@users.noreply.github.com>
2025-01-28 16:57:40 +1100

    Complete removeFromArray function to spec
    All tests passed (8)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...toRemove) {
+    for (const remove of toRemove) {
+        currentArrayLength = inputArray.length;
+        for (let i = 0; i < currentArrayLength; i++) {
+            if (inputArray[i] === remove) {
+                inputArray.splice(i, 1);
+                i--;
+                currentArrayLength--;
+            }
+        }
+    }
+    return inputArray;
 };
 
 // Do not edit below this line
