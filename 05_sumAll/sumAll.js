cbbdf1f5f432efc9cf3ee2cdc3404ea92ddc300e
JackMa222 <128107480+JackMa222@users.noreply.github.com>
2025-01-28 17:03:59 +1100

    Complete sumAll function to spec
    All tests passed (7)

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(startingInt, endingInt) {
+    let finalInt = 0;
 
+    const ints = [startingInt, endingInt];
+
+    for (const integer of ints) {
+        if (!Number.isInteger(integer)) {
+            return "ERROR";
+        }
+
+        if (integer < 0) {
+            return "ERROR";
+        }
+    }
+
+    if (startingInt > endingInt) {
+        temp = startingInt;
+        startingInt = endingInt;
+        endingInt = temp;
+    }
+
+    for (let i = startingInt; i <= endingInt; i++) {
+        finalInt += i;
+    }
+
+    return finalInt;
 };
 
 // Do not edit below this line
