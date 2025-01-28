3761e88b9bfb7acaf651f154d58d7e4a77d50d6e
JackMa222 <128107480+JackMa222@users.noreply.github.com>
2025-01-28 16:49:38 +1100

    Complete reverseString function to spec
    Passed all tests (4)

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(inputString) {
+    const stringArray = inputString.split("");
+    let newString = "";
 
+    for (i = 0; i < stringArray.length; i++) {
+        newString += stringArray[stringArray.length - i - 1]
+    }
+    return newString;
 };
 
 // Do not edit below this line
