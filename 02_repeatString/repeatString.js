b06b8e82bc5d912fe8a0720f38a36a4ddcbf2139
JackMa222 <128107480+JackMa222@users.noreply.github.com>
2025-01-28 16:44:42 +1100

    Complete repeatString function to spec
    All tests passed (7 total)

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(inputString, iterations) {
+    if (iterations < 0) {
+        return "ERROR";
+    }
+    
+    let finalString = "";
+    for (let i = 0; i < iterations; i++) {
+        finalString += inputString;
+    }
+    return finalString;
 };
 
 // Do not edit below this line
