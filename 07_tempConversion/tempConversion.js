bfb168937255a936e448f6c1cc20e3a4ee22b1ac
JackMa222 <128107480+JackMa222@users.noreply.github.com>
2025-01-28 17:12:59 +1100

    Complete convertToCelsius, convertToFahrenheit
    All tests passed (6)

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToFahrenheit = function(fahrenheitInput) {
+  let conversion = ((fahrenheitInput * (9/5)) + 32);
+
+  return Math.round(conversion * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToCelsius = function(celsiusInput) {
+  let conversion = (celsiusInput - 32) * 5/9;
+
+  return Math.round(conversion * 10) / 10;
 };
 
 // Do not edit below this line
